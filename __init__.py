1eb2882fa708ffc1656c730ce8a5085ecd5db6dc
Chris Lee <chrislee131499@gmail.com>
2025-03-30 16:54:17 -0700

    sess

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from py4web import action
 
-
+session = Session()
 # example index page using session, template and vue.js
 @action("index")  # the function below is exposed as a GET action
 @action.uses("index.html", session)  # we use the template index.html and session
