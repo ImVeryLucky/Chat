92da43ae72ebc00975f9d145b8fdab389fb012df
Chris Lee <chrislee131499@gmail.com>
2025-03-31 07:44:48 -0700

    spagettti fied

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ from flask_cors import CORS
 app = Flask(__name__, template_folder="templates")
 CORS(app)
 
-counter = 0  # Initial counter value
-user_input = ""
+#counter = 0  # Initial counter value
+#user_input = ""
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Serve frontend
+    return render_template('index.html') 
+
+@app.route('/', methods = ['POST'])
+def home_post():
+    data = request.form['text']
+    return render_template('index.html', response = data)
 
 @app.route('/update', methods=['POST'])
 def update_button():
@@ -26,17 +31,16 @@ def get_count():
 @app.route('/', methods=['POST'])
 def process():
     global user_input
-    data = request.form
-    user_input = data.get("text")  
+    data = request.form['text']
+    #user_input = data.get("text")  
    #response_text = f"You entered: {user_input}" 
-    print(data)
-    return None
+    return render_template('index.html', response = data)
 
 
 
-@app.route('/fetch', methods=['GET'])
-def fetch():
-    return jsonify({"response": user_input})
+#@app.route('/fetch', methods=['GET'])
+#def fetch():    
+#    return jsonify({"response": user_input})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000, debug=True)  # Render runs on port 10000
